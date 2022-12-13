a6d6aed7ba00fb614d396fd0e85a8906227f4976
zebafathima2004 <117371677+zeba2004@users.noreply.github.com>
2022-12-13 08:26:42 +0000

    problem12 of set01

--- set01/problem12.c
+++ set01/problem12.c
@@ -1,8 +1,11 @@
-#include<stdio.h>
-struct complex {
-	int real,img;
+#include <stdio.h>
+
+struct _complex {
+   float real;
+   float imaginary;
 };
-typedef struct complex Complex;
+
+typedef struct _complex Complex;
 
 int get_n();
 Complex input_complex();
@@ -11,46 +14,57 @@ Complex add(Complex a, Complex b);
 Complex add_n_complex(int n, Complex c[n]);
 void output(int n, Complex c[n], Complex result);
 
-int main(){
-  int n = get_n();
+int main() {
+  int n;
+  n = get_n();
   Complex c[n],result;
   input_n_complex(n,c);
   result = add_n_complex(n,c);
   output(n,c,result);
+  return 0;
 }
 
-int get_n(){
-  int a;
-  printf("Enter a number.\n");
-  scanf("%d", &a);
-  return(a);
+int get_n() {
+  int index;
+  printf("Enter the number of complex numbers: ");
+  scanf("%d",&index);
+  return index;
 }
 
-Complex input_complex(){
- Complex a;
-  printf("Enter a and b where a + ib is the first complex number.\n");
-   scanf("%d%d", &a.real, &a.img);
-  return(a);
+Complex input_complex() {
+  Complex number;
+  printf("Enter a complex number: ");
+  scanf("%f %f", &number.real, &number.imaginary);
+  return number;
 }
 
-void input_n_complex(int n, Complex c[n]){
+void input_n_complex(int n, Complex c[n]) {
   int i;
-  for(i = 0;i<n;i++){
-    c[i] = input_complex();
+  for (i=0;i<n;i++) {
+    c[i]=input_complex();
   }
 }
 
-Complex add(Complex a, Complex b){
-  Complex sum;
-  sum.real = a.real+b.real;
-  sum.img = a.img+b.img;
-  return(sum);
+Complex add(Complex a, Complex b) {
+  Complex addition;
+  addition.real = a.real + b.real;
+  addition.imaginary = a.imaginary + b.imaginary;
+  return addition;
 }
 
-Complex add_n_complex(int n, Complex c[n]){
+Complex add_n_complex(int n, Complex c[n]) {
   int i;
-  Complex sum = {0,0};
-  for(i = 0;i<n;i++){
-    sum = add(sum,c[i]);
+  Complex sum={0,0};
+  for (i=0;i<n;i++) {
+    sum=add(c[i],sum);
   }
+  return sum;
+}
+
+void output(int n, Complex c[n], Complex result) {
+  int i;
+  for(i=0;i<n-1;i++) {
+      printf("%.0f+%.0fi + ",c[i].real,c[i].imaginary);
+  }
+  printf("%.0f+%.0fi is %.0f+%.0fi \n",c[i].real,c[i].imaginary,result.real,result.imaginary);
 }
--- set03/problem02.c
+++ set03/problem02.c
@@ -7,7 +7,7 @@ int main(){
   int result;
   input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
   result=is_triangle(x1,y1,x2,y2,x3,y3);
-  output(x1,y1,x2,y2,x3,y3,result);
+  output(x1,y1,x2,y2,x3,y3,result);}
 void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
   printf("enter x1,y1");
   scanf("%f %f",x1,y1);
@@ -17,5 +17,25 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
   scanf("%f %f",x3,y3);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
-  
+  float a,b,c;
+  int result;
+  a=sqrt( (x2-x1) * (x2-x1) - (y2-y1) * (y2-y1));
+  b=sqrt( (x3-x2) * (x3-x2) - (y3-y2) * (y2-y1));
+  c=sqrt( (x3-x1) * (x3-x1) - (y3-y1) * (y3-y1));
+  if(a+b>c && b+c>a && c+a>b){
+     result=1;
+    
+  }
+  else{
+      result=0;
+  }
+  return result;
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result){
+  if(result=1){
+    printf("triangle with points %f,%f and %f,%f and %f,%f is triangle",x1,y1,x2,y2,x3,y3);
+  }
+  else {
+    printf("not a triangle");
+  }
 }
