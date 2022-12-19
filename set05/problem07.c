f9bcbe86d6fbe8f5d69186eb3d398938637e6490
zebafathima2004 <117371677+zeba2004@users.noreply.github.com>
2022-12-19 07:01:31 +0000

    problem07 of set05

--- set05/problem07.c
+++ set05/problem07.c
@@ -8,38 +8,32 @@ int main(){
   int res;
   input(&name);
   res=has_nice_name(&c);
-  output(res);
-}
+  output(res);}
+
 void input(char *name){
 printf("enter the name");
-scanf("%c",name);
-}
+scanf("%c",name);}
+
 int has_nice_name(char *c){
   int count=0;
   int count1=0;
   for(int i=0;i<=50;i++){
     if (c=='a'|| c=='e' || c=='o' || c=='i' || c=='u'){
-      count++;
-    }
+      count++;}
     else{
-      count1++;
-    }
+      count1++;}
   }
-  return count,count1;
-  
-}
+  return count,count1;}
+
 void output(int res){
   int count,count1;
   if(count>=2 && count1>=2){
-    res=1;
-  }
+    res=1;}
   else{
-    res=0;
-  }
+    res=0;}
+  
   if(res==1){
-    printf("The camel has a nice name");
-  }
+    printf("The camel has a nice name");}
   else{
-    printf("The camel does not have a nice name");
-  }
+    printf("The camel does not have a nice name");}
 }
