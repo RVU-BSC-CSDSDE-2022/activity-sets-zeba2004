a594a534d7fc6ecc6e3b3c18a5f4ad19efc04b7c
zebafathima2004 <117371677+zeba2004@users.noreply.github.com>
2022-12-19 06:59:40 +0000

    problem10 of set01

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,48 +1,39 @@
 #include<stdio.h>
-#include<string.h>
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
 int main(){
-  char string1[10],string2[10];
-int result;
-   input_two_strings( &string1,  &string2);
-  result= stringcompare( &string1,  &string2);
- output( &string1,  &string2,  result);
+  char string1[50];
+  char string2[50];
+  int result;
+  input_two_strings(&string1,&string2);
+  result=stringcompare(&string1,&string2);
+  output(&string1,&string2,result);
 }
 void input_two_strings(char *string1, char *string2){
-  printf("enter string1:\t");
+  printf("enter string1 :");
   scanf("%s",string1);
-  printf("enter string2:\t");
+  printf("enter string2 :");
   scanf("%s",string2);
-   return string1,string2;
 }
 int stringcompare(char *string1, char *string2){
-  int i,result;
-  for(i=0;string1[i]!='\0' || string2[i]!='\0';i++){
-    if(string1[i]!=string2[i]){
-      if(string1[i]<string2[i]){
-        return-1;
-      }
-      else{
-        return +1;
-      }
-    }
-   else{
-     return 0;
-   } 
+  int result;
+  for(int i=0;i<50;i++){
+  if(string1[i]>string2[i]){
+    result=+1;
   }
-  
-}
-void output(char *string1, char *string2, int result){
-  if(result==0){
-    printf("both words are same");
-  }
-  else if(result==-1){
-    
-    printf("%s is greater than %s",string2,string1);
+  else if(string1[i]<string2[i]){
+    result=-1;
   }
   else{
-    printf("%s is greater than %s",string1,string2);
+    result=0;
+  }  }
+  return result;
   }
-}
+void output(char *string1, char *string2, int result){
+  if(result==0){
+    printf("same");
+  }
+  else if(result==-1){
+    printf("%s is greater than %s",string2,string1);
+  }
